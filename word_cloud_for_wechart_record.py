217407b0fca61c0cc7702e3c6c657fa6f15f77cc
Ronald Huggler <ronald@huggler.com.br>
2017-10-19 14:07:26 -0200

    Add support for background picture

--- word_cloud_for_wechart_record.py
+++ word_cloud_for_wechart_record.py
@@ -7,13 +7,16 @@ import numpy as np
 from PIL import Image
 
 #读取一个txt文件
-text = open('how to choose the first open source project.txt','r').read()
+# Context managers automatically close the file after all operations finished
+with open('how to choose the first open source project.txt', 'r') as f:
+    text = f.read()
 
 #读入背景图片
-bg_pic = np.array(Image.open('human.png'))
+mask_pic = np.array(Image.open('human.png'))
 
-#生成词云
-wordcloud = WordCloud(mask=bg_pic,background_color='black',scale=1.5, font_path=r'C:\Windows\Fonts\STHUPO.TTF').generate_from_text(text)
+# Mode RGBA and background color None makes the background transparent, that way we can paste the resulting image
+# into another one, which will be the background
+wordcloud = WordCloud(mask=mask_pic, background_color=None, scale=1.5, mode='RGBA', font_path=r'C:\Windows\Fonts\STHUPO.TTF').generate_from_text(text)
 
 #显示词云图片
 plt.imshow(wordcloud)
@@ -21,4 +24,13 @@ plt.axis('off')
 plt.show()
 
 #保存图片
-wordcloud.to_file('test.jpg')
+# Using .png to support transparency
+wordcloud.to_file('test.png')
+
+foreground = Image.open('test.png')
+background = Image.open('background.jpg')
+
+background.paste(foreground, (0,0), foreground)
+background.show()
+
+background.save('test_with_bg.png')
