08c9cd6d719526eeca6b29b8b0561cb02571f5a4
Ronald Huggler <ronald@huggler.com.br>
2017-10-19 14:35:10 -0200

    Add input for files and background resizing

--- word_cloud_for_wechart_record.py
+++ word_cloud_for_wechart_record.py
@@ -1,36 +1,59 @@
 # -*- coding: utf-8 -*-
 
 #导入wordcloud模块和matplotlib模块
-from wordcloud import WordCloud
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 from PIL import Image
+from wordcloud import WordCloud
 
-#读取一个txt文件
-# Context managers automatically close the file after all operations finished
-with open('how to choose the first open source project.txt', 'r') as f:
-    text = f.read()
 
-#读入背景图片
-mask_pic = np.array(Image.open('human.png'))
+def generate_wordcloud(words_file, mask_pic, bg_pic, font_file=None):
+    #读取一个txt文件
+    # Context managers automatically close the file after all operations finished
+    with open(words_file, 'r') as f:
+        text = f.read()
 
-# Mode RGBA and background color None makes the background transparent, that way we can paste the resulting image
-# into another one, which will be the background
-wordcloud = WordCloud(mask=mask_pic, background_color=None, scale=1.5, mode='RGBA', font_path=r'C:\Windows\Fonts\STHUPO.TTF').generate_from_text(text)
+    #读入背景图片
+    mask_pic = np.array(Image.open(mask_pic))
 
-#显示词云图片
-plt.imshow(wordcloud)
-plt.axis('off')
-plt.show()
+    # Mode RGBA and background color None makes the background transparent, that way we can paste the resulting image
+    # into another one, which will be the background
+    wordcloud = WordCloud(mask=mask_pic, background_color=None, scale=1.5, mode='RGBA').generate_from_text(text)
 
-#保存图片
-# Using .png to support transparency
-wordcloud.to_file('test.png')
+    #显示词云图片
+    plt.imshow(wordcloud)
+    plt.axis('off')
+    plt.show()
 
-foreground = Image.open('test.png')
-background = Image.open('background.jpg')
+    #保存图片
+    # Using .png to support transparency
+    wordcloud.to_file('foreground.png')
 
-background.paste(foreground, (0,0), foreground)
-background.show()
+    foreground = Image.open('foreground.png')
+    background = Image.open(bg_pic)
 
-background.save('test_with_bg.png')
+    width, height = foreground.size
+    background = background.resize((width, height), Image.ANTIALIAS)
+
+    background.paste(foreground, (0,0), foreground)
+    background.show()
+
+    background.save('wordcloud.png')
+
+
+if __name__ == '__main__':
+    words_file = raw_input('File to read text from: ')
+    if not words_file:
+        words_file = 'how to choose the first open source project.txt'
+    
+    mask_pic = raw_input('Mask picture path: ')
+    if not mask_pic:
+        mask_pic = 'human.png'
+
+    bg_pic = raw_input('Background picture path: ')
+    if not bg_pic:
+        bg_pic = 'background.jpg'
+
+    font_file = raw_input('Custom font path (optional): ')
+
+    generate_wordcloud(words_file, mask_pic, bg_pic, font_file)
