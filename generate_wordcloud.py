c8ab57eadb6d2654e38905bc0076d1b65506c3cd
ggang <ggang@email.com>
2017-11-09 17:05:17 +0800

    1、support to show chinese

--- generate_wordcloud.py
+++ generate_wordcloud.py
@@ -5,51 +5,52 @@ import numpy as np
 #import matplotlib.pyplot as plt
 import os
 import sys
+import jieba
 from PIL import Image
 from wordcloud import WordCloud
 import ConfigParser
 
-def get_config_from_file(config_file='./config/config.ini'):
+def get_config_from_file(config_file='config.ini'):
     #Get config information from config_file, default it included in ./config/config.ini
     cf = ConfigParser.ConfigParser()
     cf.read(config_file)
     words_file = cf.get("base_config", "words_file")
-    mask_pic   = cf.get("base_config", "mask_pic")
-    bg_pic     = cf.get("base_config", "bg_pic")
-    font_file  = None
-
-    if "window_config" in cf.sections():
-        font_file =  cf.get("window_config", "font_file")
-
+    mask_pic   = cf.get("base_config", "mask_pic") if cf.has_option("base_config", "mask_pic") else None
+    bg_pic     = cf.get("base_config", "bg_pic")  if cf.has_option("base_config", "bg_pic") else None
+    font_file  = cf.get("window_config", "font_file") if cf.has_section("window_config") else None
+    
     return words_file, mask_pic, bg_pic, font_file
 
-def generate_wordcloud(words_file, mask_pic, bg_pic=None, font_file=r'C:\Windows\Fonts\COOPBL.TTF'):
-    #Read a txt file
+def generate_wordcloud(words_file, mask_pic=None, bg_pic=None, font_file=r'C:\Windows\Fonts\STLITI.TTF'):
+    #Read a txt file                                                                     
     # Context managers automatically close the file after all operations finished
+    #font_file = None
     print "%s, %s, %s, %s" % (words_file, mask_pic, bg_pic, font_file)
 
     with open(words_file, 'r') as f:
         text = f.read()
+        word_jieba = jieba.cut(text,cut_all=True)
+        word_split = " ".join(word_jieba)
 
-    #读入背景图片
-    mask_pic = np.array(Image.open(mask_pic))
-
+    if mask_pic:
+        #读入背景图片
+        mask_pic = np.array(Image.open(mask_pic))
+        wordcloud = WordCloud(mask=mask_pic, background_color=None, scale=1.5, mode='RGBA', font_path=font_file).generate(word_split)
+        
     # Mode RGBA and background color None makes the background transparent, that way we can paste the resulting image
     # into another one, which will be the background
-    if not font_file:
-        wordcloud = WordCloud(mask=mask_pic, background_color=None, scale=1.5, mode='RGBA').generate_from_text(text)
     else:
-        wordcloud = WordCloud(mask=mask_pic, background_color=None, scale=1.5, mode='RGBA', font_path=font_file).generate_from_text(text)
+        wordcloud = WordCloud(max_words=100,width = 1200,height=800, mode='RGBA', background_color=None, font_path=font_file).generate(word_split)
+    
 
     # 保存图片
     # Using .png to support transparency
     wordcloud.to_file('wordcloud.png')
 
     #显示词云图片
-    if None == bg_pic:
+    if not bg_pic:
         Image.open('wordcloud.png').show()
-
-    if bg_pic:
+    else:
         foreground = Image.open('wordcloud.png')
         background = Image.open(bg_pic)
 
@@ -62,5 +63,5 @@ def generate_wordcloud(words_file, mask_pic, bg_pic=None, font_file=r'C:\Windows
 
 
 if __name__ == '__main__':
-    words_file, mask_pic, bg_pic, font_file = get_config_from_file("./config/config.ini")
+    words_file, mask_pic, bg_pic, font_file = get_config_from_file("config.ini")
     generate_wordcloud(words_file, mask_pic, bg_pic, font_file)
