2537a724ecdd42e0f2fc64e61953a263665785b2
ggang <ggang@email.com>
2017-11-13 10:31:32 +0800

    Fix bug: #6 modified convert_to_exe.py, Make sure it be able packaged correctly

--- convert_to_exe.py
+++ convert_to_exe.py
@@ -1,6 +1,6 @@
 import os
 
-os.system("pyinstaller --add-data C:\Python27\Lib\site-packages\wordcloud\stopwords;.\wordcloud -F -w generate_wordcloud.py")
+os.system("pyinstaller --add-data C:\Python27\Lib\site-packages\wordcloud\stopwords;.\wordcloud -F generate_wordcloud.py")
 os.system("copy config.ini dist\config.ini")
 os.system("md dist\config")
 os.system("copy config\* dist\config")
